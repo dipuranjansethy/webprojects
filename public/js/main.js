77e0dfa7790b70029518b6f66d8ba8bdb0ec3e77
dipuranjansethy <dipuranjansethy1432@gmail.com>
2023-08-19 17:00:39 +0530

    second commit

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,23 @@ const city_name=document.getElementById('city_name');
 const temp_status=document.getElementById("temp_status");
 const temp=document.getElementById("temp");
 const datahide=document.querySelector('.middle_layer');
+let day=document.getElementById('day');
+const weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d=new Date();
+day.innerText=weekday[d.getDay()];
+const today_data=document.getElementById('today_data');
+const month=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+today_data.innerText=`${d.getDate()}  ${month[d.getMonth()]}`;
+
 
 const getInfo=async(event)=>{
     event.preventDefault();
     let cityval =cityName.value;
+    
+
     if (cityval=== "") {
         city_name.innerText=`Plz write something to search`;
-        datahide.classList.add('data_hide');    
+        datahide.classList.add('data_hide');
 
     }else{
         try{
@@ -19,9 +29,11 @@ const getInfo=async(event)=>{
             const data=await responce.json();
             const arrData=[data];
             city_name.innerText=`${arrData[0].name},${arrData[0].sys.country} `;
-            temp.innerText=arrData[0].main.temp;
+            const tempp=(arrData[0].main.temp-273).toFixed(2);
+            temp.innerText=`${tempp} C`;
             temp_status.innerText=arrData[0].weather[0].main;
-            console.log(data);
+            
+            
 
             if(temp_status=="Clear"){
                 temp_status.innerHTML= "<i class='fas fa-sun' style='#eccc68';></i>"
